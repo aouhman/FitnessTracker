6900b89ff710cc07977cb51f122df2c1c798d6d6
aouhman <abdessamad.ouhman@gmail.com>
2016-07-04 21:14:51 +0000

    Partie 2 : creation la classe repo et un interface  pour sauvgarder un objet de type goal

---
+++ src/main/java/com/pluralsight/repository/GoalRepository.java
@@ -0,0 +1,12 @@
+package com.pluralsight.repository;
+
+import com.pluralsight.model.Goal;
+
+/**
+ * Created by admin on 04/07/2016.
+ */
+public interface GoalRepository {
+
+    Goal save(Goal goal);
+
+}
