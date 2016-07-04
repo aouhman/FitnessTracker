50a1b3cce2ce3ff3b97fe5634c44c32520a537df
aouhman <abdessamad.ouhman@gmail.com>
2016-07-04 00:08:52 +0000

    Specifier le chemin de fichier de configuration jpaContext  et utiliser  l'annotation  pour definit les Champs id et la classe de table

--- src/main/java/com/pluralsight/model/Goal.java
+++ src/main/java/com/pluralsight/model/Goal.java
@@ -2,12 +2,29 @@ package com.pluralsight.model;
 
 import org.hibernate.validator.constraints.Range;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 
+@Entity
 public class Goal {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	@Range(min = 1, max = 120)
 	private int minutes;
+
+
 
 	public int getMinutes() {
 		return minutes;
@@ -16,5 +33,5 @@ public class Goal {
 	public void setMinutes(int minutes) {
 		this.minutes = minutes;
 	}
-	
+
 }
